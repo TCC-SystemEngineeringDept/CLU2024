e558a211577d404c1cf0c684f2cc2b8e97a75045
yoshzawa <yoshzawa@gmail.com>
2023-06-08 10:21:57 +0900

    WebAPI関連をGetLanguageクラスに移動した

--- src/main/java/jp/ac/jc21/jk3a00/detection/LangDetectionServlet.java
+++ src/main/java/jp/ac/jc21/jk3a00/detection/LangDetectionServlet.java
@@ -1,9 +1,6 @@
 package jp.ac.jc21.jk3a00.detection;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.URISyntaxException;
-import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,13 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
-
-import jp.ac.jc21.jk3a00.AzureApiDefinitionInterface;
-import jp.ac.jc21.jk3a00.api.WebApiConnector;
-import jp.ac.jc21.jk3a00.api.detection.request.LanguageDetectionRequestJson;
-import jp.ac.jc21.jk3a00.api.detection.response.LanguageDetectresponse;
+import jp.ac.jc21.jk3a00.api.detection.request.GetLanguage;
 
 /**
  * Servlet implementation class LangDetectionFormServlet
@@ -38,55 +29,12 @@ public class LangDetectionServlet extends HttpServlet {
 		}
 		request.setAttribute("parameter",parameter);
 
-		// jsonパラメーターを作成
-		LanguageDetectionRequestJson detectReq=new LanguageDetectionRequestJson(parameter);
-		Gson gson=new Gson();
-		String detectReqString = gson.toJson(detectReq);
-
-		// jsonパラメーターを表示
-		System.out.append(detectReqString);
-
-		// APIのURLを設定
-
-		String url = AzureApiDefinitionInterface.apiUrl+
-		"/language/:analyze-text?api-version=2022-05-01";
-
-		// Subscription-Keyを設定
-		HashMap< String , String >  map = new HashMap<>();
-		map.put("Ocp-Apim-Subscription-Key", AzureApiDefinitionInterface.subscriptionKey);
-
-		// Proxy設定（学内利用のみ）
-		InetSocketAddress proxy = new InetSocketAddress("172.17.0.2", 80);
-
-		LanguageDetectresponse detectResponse=null;
-		try {
-			JsonReader reader;
-			reader = WebApiConnector.postJsonReader(url, proxy, map, detectReqString);
-			if (reader != null) {
-				detectResponse = gson.fromJson(reader, LanguageDetectresponse.class);
-				reader.close();
-			}
-		} catch (URISyntaxException e) {
-			System.out.println("URISyntaxException");
-			e.printStackTrace(System.out);
-		} catch (IOException e) {
-			System.out.println("IOException");
-			e.printStackTrace(System.out);
-		} catch (InterruptedException e) {
-			System.out.println("InterruptedException");
-			e.printStackTrace(System.out);
-		}
-
-		String detected="エラーが発生しているようです。resultsがnullでした";
-		if(detectResponse != null) {
-			if(detectResponse.results != null) {
-			detected=detectResponse.results.documents[0].detectedLanguage.name;
-			}
-		}
+		String detected = GetLanguage.getLanguageText(parameter);
 		request.setAttribute("detected",detected);
 
 
 		request.getRequestDispatcher("/jsp/langDetection.jsp").forward(request,response);
 		}
+
 
 }
